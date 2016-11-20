6c5000fb34dc0448fd323fce70ba223a73fea146
Thomas GRANJON <thomas.granjon@orange.fr>
2016-11-20 08:32:46 +0100

    Ecriture de Soldat (20/11/16)

--- IConfig.java
+++ IConfig.java
@@ -12,5 +12,4 @@ public interface IConfig {
 	Color COULEUR_TEXTE = Color.black, COULEUR_MONSTRES = Color.black;
 	Color COULEUR_HEROS = Color.red, COULEUR_HEROS_DEJA_JOUE = Color.pink;
 	Color COULEUR_EAU = Color.blue, COULEUR_FORET = Color.green, COULEUR_ROCHER = Color.gray;
-	Color COULEUR_OBSTACLE = Color.yellow, COULEUR_SOLDAT = Color.orange;
 }
--- Soldat.java
+++ Soldat.java
@@ -1,15 +1,20 @@
 package wargame;
- 
+
+import java.awt.Color;
 
 public class Soldat extends Element{
 	
-    public Soldat() {
-        // TODO Auto-generated constructor   stub
-    	/*Ajouter super() dans le constructeur de soldat*/
-    }
-    
+	public enum TypeSoldat {
+		HEROS (COULEUR_HEROS), MONSTRES (COULEUR_MONSTRES), HEROS_DEJA_JOUE (COULEUR_HEROS_DEJA_JOUE);
+		private final Color COULEUR;
+		TypeSoldat(Color couleur) { COULEUR = couleur; }
+	}
+	private TypeSoldat TYPE;
+	public Soldat(){}
+    public Soldat(TypeSoldat type, Position pos) { super(); TYPE = type; this.pos = pos; }    
 	public void joueTour(int tour){}
 	public void combat(Soldat soldat){}
 	public void seDeplace(Position newPos){}
+	public String toString() { return ""+TYPE; }
 
 }
